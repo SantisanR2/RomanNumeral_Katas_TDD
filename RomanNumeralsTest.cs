d840fc13d7b28ebdffcfed25485c22c9aaa5e9bf
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2025-10-31 10:10:00 -0500

    🟥 Si_Ingreso_1_Debe_Dar_I

--- RomanNumeralsTest.cs
+++ RomanNumeralsTest.cs
@@ -1,10 +1,19 @@
-﻿namespace RomanNumerals;
+﻿using AwesomeAssertions;
+
+namespace RomanNumerals;
 
 public class RomanNumeralsTest
 {
     [Fact]
-    public void Test1()
+    public void Si_Ingreso_1_Debe_Dar_I()
     {
-        
+        var romanNumber = ToRoman(1);
+
+        romanNumber.Should().Be("I");
+    }
+
+    private string ToRoman(int i)
+    {
+        throw new NotImplementedException();
     }
 }
