0f20dce9893d54180103b0ccc2d4aab78b06c8ba
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2025-10-31 10:28:39 -0500

    🟥 Si_IngresoUnNumeroArabico_Debe_DarSuNumeroRomano

--- RomanNumeralsTest.cs
+++ RomanNumeralsTest.cs
@@ -9,6 +9,7 @@ public class RomanNumeralsTest
     [InlineData(2, "II")]
     [InlineData(5, "V")]
     [InlineData(4, "IV")]
+    [InlineData(6, "VI")]
     public void Si_IngresoUnNumeroArabico_Debe_DarSuNumeroRomano(int arabicNUmber, string expectedRomanNumber)
     {
         var romanNumber = ToRoman(arabicNUmber);
